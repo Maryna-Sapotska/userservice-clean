acadaac8c51ec45d070db9e42b71950ce6198fd9
Maryna Sapotska <snaczkaya@mail.ru>
2026-05-27 17:54:18 +0300

    fix: add logging to global exception handler

--- src/main/java/com/innowise/userservice/exception/GlobalExceptionHandler.java
+++ src/main/java/com/innowise/userservice/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.innowise.userservice.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -35,9 +37,12 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleGeneral(Exception ex){
+
+        log.error("Unexpected error occurred", ex);
+
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body("Unexpected error: " + ex.getMessage());
+                .body("Internal server error");
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
