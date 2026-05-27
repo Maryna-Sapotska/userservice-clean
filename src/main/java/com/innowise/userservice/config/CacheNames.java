fa5ef044bcb4b6e9244bd1e48692636aae776e7a
Maryna Sapotska <snaczkaya@mail.ru>
2026-05-27 18:15:55 +0300

    fix: add private constructor to utility class CacheNames

--- src/main/java/com/innowise/userservice/config/CacheNames.java
+++ src/main/java/com/innowise/userservice/config/CacheNames.java
@@ -1,6 +1,11 @@
 package com.innowise.userservice.config;
 
 public final class CacheNames {
+
+    private CacheNames() {
+        throw new IllegalStateException("Utility class");
+    }
+
     public static final String USERS = "users";
     public static final String USERS_WITH_CARDS = "users_with_cards";
     public static final String CARDS = "cards";
