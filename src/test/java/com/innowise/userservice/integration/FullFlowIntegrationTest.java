769699f223dda78a5ea28ff3848fbdff823ba70a
Maryna Sapotska <snaczkaya@mail.ru>
2026-05-27 15:33:33 +0300

    fix: add import

--- src/test/java/com/innowise/userservice/integration/FullFlowIntegrationTest.java
+++ src/test/java/com/innowise/userservice/integration/FullFlowIntegrationTest.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDate;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
