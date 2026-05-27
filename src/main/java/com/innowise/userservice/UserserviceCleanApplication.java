1958baef6ce985f50422496ae84c567681a4956d
Maryna Sapotska <snaczkaya@mail.ru>
2026-05-27 14:27:33 +0300

    init

---
+++ src/main/java/com/innowise/userservice/UserserviceCleanApplication.java
@@ -0,0 +1,13 @@
+package com.innowise.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserserviceCleanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserserviceCleanApplication.class, args);
+    }
+
+}
